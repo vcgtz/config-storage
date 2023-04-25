297e95d9bac5eed6a59fa999d64207686fc112f8
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-04-24 20:37:23 -0600

    Improve typing in tests

--- tests/ConfigurationStorage.test.ts
+++ tests/ConfigurationStorage.test.ts
@@ -40,9 +40,9 @@ describe('Testing storing and retrieving data', () => {
   });
 
   test('Storing a single string', async () => {
-    const key = 'message';
-    const value = 'Hello World!';
-    const expectedObj = { message: value };
+    const key: string = 'message';
+    const value: string = 'Hello World!';
+    const expectedObj: any = { message: value };
 
     await storage.set(key, value);
 
@@ -51,9 +51,9 @@ describe('Testing storing and retrieving data', () => {
   });
 
   test('Storing a single number', async () => {
-    const key = 'pi';
-    const value = 3.1416;
-    const expectedObj = { pi: value };
+    const key: string = 'pi';
+    const value: number = 3.1416;
+    const expectedObj: any = { pi: value };
 
     await storage.set(key, value);
 
@@ -62,9 +62,9 @@ describe('Testing storing and retrieving data', () => {
   });
 
   test('Storing a single boolean', async () => {
-    const key = 'enabled';
-    const value = false;
-    const expectedObj = { enabled: value };
+    const key: string = 'enabled';
+    const value: boolean = false;
+    const expectedObj: any = { enabled: value };
 
     await storage.set(key, value);
 
@@ -73,40 +73,43 @@ describe('Testing storing and retrieving data', () => {
   });
 
   test('Storing a nested object with 6 levels', async () => {
-    const key1 = 'obj1.obj2.obj3.obj4.obj5.obj6';
-    const value1 = 'Hello World!';
-    const key2 = 'obj1.obj2.obj3.obj4';
-    const value2 = { obj5: { obj6: value1 } };
-    const expectedObj = { obj1: { obj2: { obj3: { obj4: { obj5: { obj6: value1 } } } } } };
+    const fullNestedKey: string = 'obj1.obj2.obj3.obj4.obj5.obj6';
+    const fullNestedValue: string = 'Hello World!';
+    const nestedKey: string = 'obj1.obj2.obj3.obj4';
+    const nestedValue: any = { obj5: { obj6: fullNestedValue } };
+    const expectedObj: any = { obj1: { obj2: { obj3: { obj4: { obj5: { obj6: fullNestedValue } } } } } };
 
-    await storage.set(key1, value1);
+    await storage.set(fullNestedKey, fullNestedValue);
 
-    expect(await storage.get(key1)).toBe(value1);
-    expect(await storage.get(key2)).toMatchObject(value2);
+    expect(await storage.get(fullNestedKey)).toBe(fullNestedValue);
+    expect(await storage.get(nestedKey)).toMatchObject(nestedValue);
     expect(await storage.getAll()).toMatchObject(expectedObj);
   });
 
   test('Storing a deep nested object with 3 levels', async () => {
-    const key1 = 'obj1.obj2.obj3';
-    const key2 = 'obj1.obj2.obj4';
-    const value1 = 'Hello World!';
-    const value2 = 3.1416;
-    const expectedObj = { obj1: { obj2: { obj3: value1, obj4: value2 } } };
+    const nestedKey1: string = 'obj1.obj2.obj3';
+    const nestedKey2: string = 'obj1.obj2.obj4';
+    const nestedValue1: string = 'Hello World!';
+    const nestedValue2: number = 3.1416;
+    const expectedObj: any = { obj1: { obj2: { obj3: nestedValue1, obj4: nestedValue2 } } };
 
-    await storage.set(key1, value1);
-    await storage.set(key2, value2);
+    await storage.set(nestedKey1, nestedValue1);
+    await storage.set(nestedKey2, nestedValue2);
 
-    expect(await storage.get(key1)).toBe(value1);
-    expect(await storage.get(key2)).toBe(value2);
+    expect(await storage.get(nestedKey1)).toBe(nestedValue1);
+    expect(await storage.get(nestedKey2)).toBe(nestedValue2);
     expect(await storage.getAll()).toMatchObject(expectedObj);
   });
 
   test('Storing a full object', async () => {
-    const key1 = 'obj1';
-    const key2 = 'obj1.message';
-    const key3 = 'obj1.obj2.nestedMessage';
-    const key4 = 'obj1.obj2.obj3.nestedPi';
-    const value = {
+    const mainKey: string = 'obj1';
+    const nestedKey1: string = 'obj1.message';
+    const nestedKey2: string = 'obj1.obj2.nestedMessage';
+    const nestedKey3: string = 'obj1.obj2.obj3.nestedPi';
+    const nestedValue1: string = 'Hello World!';
+    const nestedValue2: string = 'Hello World from far far away';
+    const nestedValue3: number = 3.14159;
+    const mainValue: any = {
       message: 'Hello World!',
       pi: 3.1416,
       obj2: {
@@ -117,12 +120,12 @@ describe('Testing storing and retrieving data', () => {
       },
     };
 
-    await storage.set(key1, value);
+    await storage.set(mainKey, mainValue);
 
-    expect(await storage.get(key1)).toMatchObject(value);
-    expect(await storage.get(key2)).toBe('Hello World!');
-    expect(await storage.get(key3)).toBe('Hello World from far far away');
-    expect(await storage.get(key4)).toBe(3.14159);
+    expect(await storage.get(mainKey)).toMatchObject(mainValue);
+    expect(await storage.get(nestedKey1)).toBe(nestedValue1);
+    expect(await storage.get(nestedKey2)).toBe(nestedValue2);
+    expect(await storage.get(nestedKey3)).toBe(nestedValue3);
   });
 
   afterAll(async () => {
@@ -135,39 +138,7 @@ describe('Testing deleting existing values', () => {
 
   beforeEach(async () => {
     storage = await ConfigurationStorage.getStorage('testing');
-    await storage.set('obj1', {
-      message: 'Hello World!',
-      pi: 3.1416,
-      obj2: {
-        nestedMessage: 'Hello World from far far away',
-        obj3: {
-          nestedPi: 3.14159,
-        },
-      },
-    });
-  });
 
-  test('Demoving a single string', async () => {
-    const key = 'message';
-    const value = 'Hello World!';
-    const expectedObj = { message: value };
-
-    await storage.set(key, value);
-
-    expect(await storage.get(key)).toBe(value);
-    expect(await storage.getAll()).toMatchObject(expectedObj);
-  });
-
-  afterAll(async () => {
-    await fsPromises.unlink(storage.path);
-  });
-});
-
-describe('Testing deleting existing values', () => {
-  let storage: ConfigurationStorage;
-
-  beforeEach(async () => {
-    storage = await ConfigurationStorage.getStorage('testing');
     await storage.set('obj1', {
       message: 'Hello World!',
       pi: 3.1416,
@@ -181,12 +152,13 @@ describe('Testing deleting existing values', () => {
   });
 
   test('Removing a single value', async () => {
-    const key = 'message';
-    const expectedObj = {};
+    const key: string = 'message';
+    const value: string = 'Hello World!';
+    const expectedObj: any = {};
 
-    await storage.set(key, 'Hello World!');
+    await storage.set(key, value);
 
-    expect(await storage.get(key)).toBe('Hello World!');
+    expect(await storage.get(key)).toBe(value);
 
     await storage.del(key);
 
@@ -194,16 +166,17 @@ describe('Testing deleting existing values', () => {
   });
 
   test('Removing all the data', async () => {
-    const key = 'obj1';
+    const key: string = 'obj1';
     const expectedObj = {};
 
     await storage.del(key);
+
     expect(await storage.getAll()).toMatchObject(expectedObj);
   });
 
   test('Removing a nested string', async () => {
-    const key = 'obj1.message';
-    const expectedObj = {
+    const key: string = 'obj1.message';
+    const expectedObj: any = {
       obj1: {
         pi: 3.1416,
         obj2: {
@@ -220,8 +193,8 @@ describe('Testing deleting existing values', () => {
   });
 
   test('Removing a nested number', async () => {
-    const key = 'obj1.obj2.obj3.nestedPi';
-    const expectedObj = {
+    const key: string = 'obj1.obj2.obj3.nestedPi';
+    const expectedObj: any = {
       obj1: {
         pi: 3.1416,
         obj2: {
@@ -241,12 +214,12 @@ describe('Testing deleting existing values', () => {
   });
 });
 
-
 describe('Testing existance of values', () => {
   let storage: ConfigurationStorage;
 
   beforeEach(async () => {
     storage = await ConfigurationStorage.getStorage('testing');
+
     await storage.set('obj1', {
       message: 'Hello World!',
       pi: 3.1416,
@@ -260,31 +233,31 @@ describe('Testing existance of values', () => {
   });
 
   test('Checking existence of a single value', async () => {
-    const key = 'obj1';
+    const key: string = 'obj1';
 
     expect(await storage.exists(key)).toBe(true);
   });
 
   test('Checking existence of a nested value', async () => {
-    const key = 'obj1.message';
+    const key: string = 'obj1.message';
 
     expect(await storage.exists(key)).toBe(true);
   });
 
   test('Checking existence of a deep nested value', async () => {
-    const key = 'obj1.obj2.obj3.nestedPi';
+    const key: string = 'obj1.obj2.obj3.nestedPi';
 
     expect(await storage.exists(key)).toBe(true);
   });
 
   test('Checking existence of a not existing value', async () => {
-    const key = 'obj2';
+    const key: string = 'obj2';
 
     expect(await storage.exists(key)).toBe(false);
   });
 
   test('Checking existence of a not existing nested value', async () => {
-    const key = 'obj2.obj20.obj29';
+    const key: string = 'obj2.obj20.obj29';
 
     expect(await storage.exists(key)).toBe(false);
   });
